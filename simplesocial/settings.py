5cf8f113677100dc8538cd32ed7de046a3cc9d0d
siddharth dewani <dewanisid@gmail.com>
2025-07-23 11:03:22 +0530

    database changed to postgres from sqlite

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'simplesocial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'social_db',
+        'USER': 'siddharthdewani',
+        'PASSWORD': '',  # leave empty if none
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
